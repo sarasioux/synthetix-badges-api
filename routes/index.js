c29ca46f06ff0c13f218e28eb29d6dc05b5ceb88
Sara Robertson <sara.melodramatic@gmail.com>
2021-05-21 13:30:32 -0400

    Update with public site url

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const Web3 = require('web3');
 const TruffleContract = require("@truffle/contract");
 
 // Constants
-const siteUrl = 'http://localhost:8080/';
+const siteUrl = 'https://synthetixbadges.netlify.app/';
 const provider = 'wss://kovan.infura.io/ws/v3/8f868fcca6aa44febce5b6a085aa23f2';
 const contractAddress = '0x22025e2b843A22cC567863C270D24da29aC7B326';               // Address of the token contract
 const ownerAccount = '0x00796e910Bd0228ddF4cd79e3f353871a61C351C';                  // Address of the contract owner
